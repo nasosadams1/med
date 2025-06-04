a5dcde9796d0c22edd89f91fc8766662cd8531bd
nasosadams1 <nasoosadamopoylos@gmail.com>
2025-06-04 15:06:17 +0300

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
+import 'react-datepicker/dist/react-datepicker.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
